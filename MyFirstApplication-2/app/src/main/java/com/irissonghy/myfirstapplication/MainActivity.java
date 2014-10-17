c72b9e709453bbd5b44f85f74fbf86197dd15095
lavanya2 <lakumar965@gmail.com>
2014-10-17 15:29:26 -0400

    added import.R statement to LoginActivity.java

--- MyFirstApplication-2/app/src/main/java/com/irissonghy/myfirstapplication/MainActivity.java
+++ MyFirstApplication-2/app/src/main/java/com/irissonghy/myfirstapplication/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
+import android.R;
 
 
 public class MainActivity extends Activity {
