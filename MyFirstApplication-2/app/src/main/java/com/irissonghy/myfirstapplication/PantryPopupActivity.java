8bf3a99f4c4addfa6b37e1ab972078ac27a5659c
lavanya2 <lakumar965@gmail.com>
2014-10-17 15:59:38 -0400

    deleted spinner in add recipe and profile page

--- MyFirstApplication-2/app/src/main/java/com/irissonghy/myfirstapplication/PantryPopupActivity.java
+++ MyFirstApplication-2/app/src/main/java/com/irissonghy/myfirstapplication/PantryPopupActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.R;
 
 
 public class PantryPopupActivity extends Activity {
